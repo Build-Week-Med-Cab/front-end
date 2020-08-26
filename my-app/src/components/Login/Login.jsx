9b13191d074c9bb6a4accadbabd3a744578a4119
cristian robles <gogo90553@icloud.com>
2020-08-25 20:47:38 -0500

    login form completed

--- my-app/src/components/Login/Login.jsx
+++ my-app/src/components/Login/Login.jsx
@@ -2,38 +2,55 @@ import React, {useState} from "react";
 import axios from 'axios';
 import loginImg from './weed.svg'
 import "./style.scss";
+import { Link } from "react-router-dom";
+
 
 
 
 export class Login extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
   }
-
-  // const [login, setLogin] = useState({
-  //   username: '',
-  //   password: ''
-  // })
-
-  // const handleChange = (e) => {
-  //   setLogin({
-  //     ...login,
-  //     [e.target.name]: e.target.value
-  //   })
-  //   console.log(login)
-  // }
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   axios
-  //   .post('https://medcabinet-ds-0820.herokuapp.com/labels/api/auth/register', login)
-  //   .then(res => {
-  //     console.log(res);
-  //     window.localStorage.setItem('token', res.data.payload)
-  //   })
   
 
+
+  
   render() {
+  
+   
+      // make a post request to retrieve a token from the api
+      // when you have handled the token, navigate to the BubblePage route
+    
+      const handleChange = (e) => {
+        this.setState({
+          login: {
+            ...this.state.login,
+          [e.target.name]: e.target.value
+          }
+        })
+        console.log(this.state.login)
+      }
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+        .post('https://med-cab3-backend-api.herokuapp.com/api/auth/login', this.state.login)
+        .then(res => {
+          console.log(res);
+          window.localStorage.setItem('token', res.data.token)
+          // props.history.push('/WebApp')
+        })
+      }
+    
+    
+     
+  
     
     return (
       <div className="base-container">
@@ -52,8 +69,8 @@ export class Login extends React.Component {
                   type="text"
                   name="username"
                   label="username"
-                  // value={login.username}
-                  // onChange={handleChange}
+                  value={this.state.login.username}
+                  onChange={handleChange}
                   className="input"
                 />
               </div>
@@ -63,15 +80,18 @@ export class Login extends React.Component {
                   type="text"
                   name="password"
                   label="password"
-                  // value={login.password}
-                  // onChange={handleChange}
+                  value={this.state.login.password}
+                  onChange={handleChange}
                   className="input"
                 />
               </div>
             </div>
           </div>
           <div className="footer">
-            <button type="button" className="btn">Login</button>
+            <Link to="/WebApp">
+            <button type="button" className="btn" onClick={handleSubmit}>Login</button>
+            </Link>
+            
           </div>
           <form/>
       </div>
--- my-app/src/components/Login/register.jsx
+++ my-app/src/components/Login/register.jsx
@@ -6,33 +6,42 @@ import "./style.scss";
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      signUp: {
+        username:'',
+        password:''
+      }
+    }
   }
+    
+  
+     
+        // make a post request to retrieve a token from the api
+        // when you have handled the token, navigate to the BubblePage route
+      
+        
 
   render() {
- 
-
-      // const [signUp, setSignUp] = useState({
-      //   username: '',
-      //   password: ''
-      // })
-    
-      // const handleChange = (e) => {
-      //   setSignUp({
-      //     ...signUp,
-      //     [e.target.name]: e.target.value
-      //   })
-      //   console.log(signUp)
-      // }
-    
-      // const handleSubmit = (e) => {
-      //   e.preventDefault();
-      //   axios
-      //   .post('https://medcabinet-ds-0820.herokuapp.com/labels/api/auth/register', signUp)
-      //   .then(res => {
-      //     console.log(res);
-      //     window.localStorage.setItem('token', res.data.payload)
-      //   })
-      
+    const handleChange = (e) => {
+      this.setState({
+        signUp: {
+          ...this.state.signUp,
+        [e.target.name]: e.target.value
+        }
+      })
+      console.log(this.state.signUp)
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      axios
+      .post('https://med-cab3-backend-api.herokuapp.com/api/auth/register', this.state.signUp)
+      .then(res => {
+        console.log(res);
+        window.localStorage.setItem('token', res.data.token)
+        // props.history.push('/WebApp')
+      })
+    }
     
     return (
       <div className="base-container">
@@ -51,8 +60,8 @@ export class Register extends React.Component {
                   type="text"
                   name="username"
                   label="username"
-                  // value={signUp.username}
-                  // onChange={handleChange}
+                  value={this.state.signUp.username}
+                  onChange={handleChange}
                   className="input"
                 />
               </div>
@@ -62,15 +71,15 @@ export class Register extends React.Component {
                   type="text"
                   name="password"
                   label="password"
-                  // value={signUp.password}
-                  // onChange={handleChange}
+                  value={this.state.signUp.password}
+                  onChange={handleChange}
                   className="input"
                 />
               </div>
             </div>
           </div>
           <div className="footer">
-            <button type="button" className="btn">Register</button>
+            <button type="button" className="btn" onClick={handleSubmit}>Register</button>
           </div>
           <form/>
       </div>
