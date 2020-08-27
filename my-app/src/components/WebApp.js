1d2f0f04fbc5fe6930197aba0f005e01d1395dc4
cristian robles <gogo90553@icloud.com>
2020-08-26 21:25:51 -0500

    working on web app

--- my-app/src/components/WebApp.js
+++ my-app/src/components/WebApp.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios'
 
 const WebApp = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    );
+
+  const [ data, setData ] = useState({})
+  
+  
+    const fetchingData = () => {
+        axios
+        .get('https://medcabinet-ds-0820.herokuapp.com/labels')
+        .then(res => {
+          
+          setData(res.data)
+          console.log(res)
+        })
+        .catch(err => console.error(err));
+        
+    }
+    console.log(data)
+
+   useEffect(() => {
+     fetchingData()
+   }, []) 
+
+
+
+ 
+  return (
+      <>
+    <Form>
+      <FormGroup>
+        <Label for="effects">Effects</Label>
+        <Input type="select" name="effects" id="exampleSelect">
+          {data !== {} && data.effects.map(effect => {
+            
+            return(
+            <option value={effect}>{effect}</option>
+          )})}
+        </Input>
+      </FormGroup>
+      </Form>
+      </>
+  );
 };
 
 export default WebApp;
